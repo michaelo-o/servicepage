70fb79297682304b196bb99ff6b767b78ea118b3
m1chae-l <mikeokwuosah@gmail.com>
2022-09-06 19:43:30 +0100

    page2

--- components/Navbar.js
+++ components/Navbar.js
@@ -2,9 +2,21 @@ import Link from 'next/link'
 import Image from 'next/image'
 import Head from 'next/head'
 import styles from '../styles/navbar.module.css'
+import { useState } from 'react'
 
 
 const Navbar = () => {
+
+    const [member, setmember] = useState(false)
+
+    const openMem = () => {
+        setmember(true)
+    }
+
+    const closeMem = () => {
+        setmember(false)
+    }
+
     return (
         <>
             <nav className={styles.navbar}>
@@ -15,7 +27,7 @@ const Navbar = () => {
                     <span>Home</span>
                     <span>Services</span>
                     <span>FAQ</span>
-                    <span>Become a member</span>
+                    <span onClick={openMem}>Become a member</span>
                 </div>
             </nav>
 
@@ -25,7 +37,17 @@ const Navbar = () => {
 
 
 
-
+            {member ? (
+                <div className={styles.memberModal}>
+                    <div className={styles.closeSection}>
+                        <button onClick={closeMem} className={styles.xbutton}><Image src="/whiteClose.svg" width={48} height={48} alt="close" /></button>
+                    </div>
+                    <div className={styles.contentSection}>
+                        <h2>Lorem ipsum dolor sit amet consectetur adipisicing elit. Id deserunt dicta, labore esse aspernatur reprehenderit h voluptatem, ullam quis officia maiores fugiat minus odit cumque dolorem non ut suscipit laboriosam. Delectus?</h2>
+                    </div>
+                </div>
+            ) : null
+            }
 
         </>
     );
